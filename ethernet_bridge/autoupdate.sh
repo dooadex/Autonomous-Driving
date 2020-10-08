933259fe1478d6787eb2cda0de5c8c2a26b62f09
dooadex <dooadex@gmail.com>
2020-10-08 00:24:31 -0700

    delete wrong uploaded dir

--- ethernet_bridge/autoupdate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm ${HOME}/apollo/docker/scripts/dev_start.sh
-cp ${HOME}/ApolloAuto/ethernet_bridge/dev_start.sh ${HOME}/apollo/docker/scripts/dev_start.sh
-cp -r ${HOME}/ApolloAuto/ethernet_bridge/ethernet ${HOME}/apollo/modules
